d76e34e3ff50656fc25ab03668f7071c5b8e321b
Zineb NAIMI <naimizineb03@gmail.com>
2025-12-05 13:44:53 +0100

    update

--- app/src/main/java/com/example/appbienvenidos/MainActivity.java
+++ app/src/main/java/com/example/appbienvenidos/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_fragment);
+        setContentView(R.layout.fragment_home);
 
         /*AppDataBase db = AppDataBase.getDatabase(this);
 
